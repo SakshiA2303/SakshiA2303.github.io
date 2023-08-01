aa30b63645addf96db196307fa09accb58dd74eb
SakshiA2303 <sakshiagnihotri162@gmail.com>
2023-08-01 19:21:33 +0530

    shoppingcart
    ecommerce site using reactjs

---
+++ e_cart/src/mainproducts.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/f5.webp";
+import product2 from "./assets/81xW62KXNhL._AC_UY327_FMwebp_QL65_.webp";
+import product3 from "./assets/81tioCUVf4L._AC_UY327_FMwebp_QL65_.webp";
+import product4 from "./assets/3s.webp";
+import product5 from "./assets/4ss.webp";
+import product6 from "./assets/h5.webp";
+import product7 from "./assets/f8.webp";
+import product8 from "./assets/n9.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: " Iphone",
+    price: 999,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2023 (M2)",
+    price: 2900,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Zebronics gaming mouse",
+    price: 99.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: " Crystal Glass Ceiling Chandelier",
+    price: 28.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Cooling pad for laptop",
+    price: 99.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Home Theater",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "SAMSUNG GALAXY S23 Ultra 5g",
+    price: 9000.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "NOISE Smart Watch",
+    price: 240.0,
+    productImage: product8,
+  },
+];
